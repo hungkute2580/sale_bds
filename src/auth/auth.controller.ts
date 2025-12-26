544ec28440dcb661a2bfbf2c794527043d78ba14
hungkute <hungkute2580@gmail.com>
2025-12-26 20:59:06 +0700

    [BDS-01] Create login/logout DTO and auth APIs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,24 @@
 import { Body, Controller, Post, Headers } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-
+import { ApiTags } from '@nestjs/swagger';
+import { LoginDto } from 'src/dto/login.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
+
+  @Post('login')
+  login(@Body() body: LoginDto) {
+    return {
+      message: 'Login OK',
+      token: 'fake-jwt-token',
+    };
+  }
+
+  @Post('logout')
+  logout(@Headers('authorization') auth: string) {
+    return {
+      message: 'Logout OK',
+    };
+  }
 
 }
