4a278eb55db447d41c1ce1b1bb31be3a3294aaa3
hungkute <hungkute2580@gmail.com>
2025-12-26 20:49:17 +0700

    Initial commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Body, Controller, Post, Headers } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+
+@ApiTags('Auth')
+@Controller('auth')
+export class AuthController {
+
+}
